a5a93493b2a43c57dbe05594ef4d573b59765a53
Azi Crawford <acrawford@trulia.com>
2014-05-24 12:59:27 -0700

    Initial Commit

---
+++ Poly-Gnome/PGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PGAppDelegate.h
+//  Poly-Gnome
+//
+//  Created by Azi Crawford on 5/24/14.
+//  Copyright (c) 2014 Azi Crawford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Poly-Gnome/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Poly-Gnome
+//
+//  Created by Azi Crawford on 5/24/14.
+//  Copyright (c) 2014 Azi Crawford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PGAppDelegate class]));
+    }
+}
